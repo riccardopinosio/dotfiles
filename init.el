3db8eb49e31769588faf59dc676d3694f2581083
riccardopinosio <rpinosio@gmail.com>
2020-05-16 09:59:04 +0000

    mods

--- init.el
+++ init.el
@@ -1331,3 +1331,15 @@ Lastly, if no tabs left in the window, it is deleted with `delete-window` functi
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-last-refresh-date "2020-05-16T09:52"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
