5bfc32b50124ddcc8489c39412f40ecbdbc29f2c
riccardo <riccardo@knightsanalytics.com>
2023-02-07 20:23:54 +0100

    elisp config code to import zotero notes

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -1,4 +1,4 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
@@ -44,7 +44,11 @@
 
 (after! org
   :custom
-  (setq org-startup-folded t)
+  (org-link-set-parameters "zotero" :follow
+                           (lambda (zpath)
+                             (browse-url
+                              (format "zotero:%s" zpath)
+                              )))
   )
 
 (after! org-roam
@@ -53,39 +57,34 @@
   (setq org-roam-directory (file-truename "~/braindump"))
   (setq org-roam-dailies-directory "daily/")
   (setq org-roam-dailies-capture-templates
-      '(("d" "default" entry
-        "* %?"
-        :target (file+head "%<%Y-%m-%d>.org"
-                           "#+title: %<%Y-%m-%d>\n"))))
+        '(("d" "default" entry
+           "* %?"
+           :target (file+head "%<%Y-%m-%d>.org"
+                              "#+title: %<%Y-%m-%d>\n"))))
   (add-to-list 'display-buffer-alist
-             '("\\*org-roam\\*"
-               (display-buffer-in-direction)
-               (direction . right)
-               (window-width . 0.33)
-               (window-height . fit-window-to-buffer)))
+               '("\\*org-roam\\*"
+                 (display-buffer-in-direction)
+                 (direction . right)
+                 (window-width . 0.33)
+                 (window-height . fit-window-to-buffer)))
   (setq org-roam-capture-templates
         '(
-            ("d" "default" plain "%?"
-            :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                               "#+title: ${title}\n<t")
-            :unnarrowed t)
-            ("m" "meeting" plain "%?"
-            :target (file+head "meetings/%<%Y%m%d%H%M%S>-${slug}.org"
-                               ":PROPERTIES:\n:project: fill\n:people: fill\n:END:\n#+title: ${title}\n#+filetags:")
-            :unnarrowed t)
-            ("t" "main" plain "%?"
-            :target (file+head "main/%<%Y%m%d%H%M%S>-${slug}.org"
-                               "#+title: ${title}\n#+filetags:")
-            :unnarrowed t)
-                ("r" "reference" plain "%?"
-                :if-new
-                (file+head "reference/${title}.org" "#+title: ${title}\n")
-                :immediate-finish t
-                :unnarrowed t)
-      ))
+          ("d" "default" plain "%?"
+           :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n<t")
+           :unnarrowed t)
+          ("m" "meeting" plain "%?"
+           :target (file+head "meetings/%<%Y%m%d%H%M%S>-${slug}.org"
+                              ":PROPERTIES:\n:project: fill\n:people: fill\n:END:\n#+title: ${title} %<%Y-%m-%d>\n#+filetags:")
+           :unnarrowed t)
+          ("t" "main" plain "%?"
+           :target (file+head "main/%<%Y%m%d%H%M%S>-${slug}.org"
+                              "#+title: ${title}\n#+filetags:")
+           :unnarrowed t)
+          ))
   :config
   (org-roam-db-autosync-enable)
-)
+  )
 
 (use-package! websocket
     :after org-roam)
@@ -105,12 +104,20 @@
 ;; CITATIONS
 
 (after! oc
-(setq org-cite-global-bibliography '("~/braindump/references.bib"))
-  )
+(setq org-cite-global-bibliography '("~/braindump/references.bib")))
 
+;;(defun riccardo/format-citar-file (file)
+;;  (let* ((parts (split-string file ";"))
+;;        (ret (cl-loop for str in parts
+;;                       ;;if (string-suffix-p ".pdf" str)
+;;                       collect str)))
+;;    (setq paths "")
+;;    (dolist (path ret) (setq paths (concat paths " " "[[" path "]]")))
+;;    paths))
 
 ;; Use `citar' with `org-cite'
 ;;
+
 (use-package! citar
   :after oc
   :custom
@@ -118,11 +125,144 @@
   (org-cite-follow-processor 'citar)
   (org-cite-activate-processor 'citar)
   (citar-bibliography '("~/braindump/references.bib"))
-;;  (citar-org-roam-note-title-template "${author}")
+  (citar-org-roam-note-title-template "${author} - ${title}\npdf: ${file}")
   :bind
   (:map org-mode-map :package org ("C-c b" . #'org-cite-insert))
   )
 
+(defadvice! riccardo/citar-file-trust-zotero (oldfun &rest r)
+  "Leave Zotero-generated file paths alone, especially zotero://..."
+  :around '(citar-file-open citar-file--find-files-in-dirs)
+  (cl-letf (((symbol-function 'file-exists-p) #'always)
+            ((symbol-function 'expand-file-name) (lambda (first &rest _) first)))
+    (apply oldfun r)))
+
+(after! citar
+  (add-to-list 'citar-file-open-functions '("pdf" . citar-file-open-external)))
+
+;; link zotero annotations
+
+(defun riccardo/grab-reference-items (citekey item-list)
+  (with-temp-buffer
+    (insert-file-contents (nth 0 citar-bibliography))
+    (let* ((citekey-start (search-forward citekey nil nil))
+           (citekey-start (if citekey-start (line-beginning-position)
+                            nil))
+           (citekey-end (if citekey-start (search-forward "@" nil nil)
+                          nil)))
+      (mapcar (lambda (item-name)
+                (let* (
+                       (item-start (if citekey-start (progn (goto-char citekey-start)
+                                                            (search-forward (format "%s = {" item-name)
+                                                                            citekey-end t))
+                                     nil
+                                     ))
+                       (item-end (if item-start (progn (up-list) (point)) nil)))
+                  (if item-start
+                      (buffer-substring-no-properties item-start item-end)
+                    "")))
+              item-list)
+    )))
+
+(defun riccardo/parse-annotation-par (el)
+  (let* ((match (string-match ".*(\\(.*\\),\\(.*\\),.*p.\s\\([0-9]+\\))\\(.*\\)" el))
+        (components (when match
+                      (list :author (match-string 1 el)
+                            :year (match-string 2 el)
+                            :page (match-string 3 el)
+                            :content (match-string 4 el)
+                            :quote (let ((match (string-match ".*``\\(.*\\)''" el)))
+                                     (when match (match-string 1 el)))
+                            )
+                      )))
+    components
+    )
+  )
+
+(defun riccardo/parse-annotation-components (annotation-section url)
+  (let* ((match (string-match ".*(\\([0-9]+\\)/\\([0-9]+\\)/\\([0-9]+\\),\s\\(.+\\))" annotation-section))
+         (annotation-attributes (list :year (match-string 3 annotation-section)
+                                :month (match-string 2 annotation-section)
+                                :day (match-string 1 annotation-section)
+                                :timestamp (match-string 4 annotation-section)))
+         (url (replace-regexp-in-string "?.*" "" url))
+         )
+    (setq annotation-pars (remq nil (mapcar #'riccardo/parse-annotation-par
+                                            (split-string annotation-section "\par[\s\n]+"))))
+    (setq annotation-pars (mapconcat (lambda (el)
+                             (let* ((quote (plist-get el :quote))
+                                    (page (plist-get el :page))
+                                    (content (plist-get el :content))
+                                    (url (concat url (format "?p%s" page))))
+                               (concat (format "*** [[%s][note p%s]]\n"
+                                               url
+                                               page)
+                                       (when quote
+                                         (format "#+BEGIN_QUOTE\n%s\n#+END_QUOTE\n" quote))
+                                       (when content content))))
+                           annotation-pars "\n"))
+    ;; format note for display
+    (format "** (%s-%s-%s), %s:\n%s"
+            (plist-get annotation-attributes :year)
+            (plist-get annotation-attributes :month)
+            (plist-get annotation-attributes :day)
+            (plist-get annotation-attributes :timestamp)
+            annotation-pars
+            )
+    ))
+
+
+(defun riccardo/parse-zotero-notes (notes url)
+  "tst"
+  (let* ((annotation-sections (split-string notes "\section"))
+        (annotation-sections (seq-filter (lambda (el) (string-match-p (regexp-quote "Annotation")
+                                                          el)) annotation-sections))
+        (notes-parsed (mapcar (lambda (el) (riccardo/parse-annotation-components el url)) annotation-sections)))
+    (mapconcat 'identity notes-parsed "\n")
+    )
+  )
+
+(defun riccardo/create-zotero-notes (notes file-url)
+  (let* ((notes (riccardo/parse-zotero-notes notes file-url))
+         (data (org-element-parse-buffer 'greater-elements))
+         (begin-zotero-headline (org-element-map data 'headline
+                          (lambda (el)
+                            (and (string= "zotero notes" (org-element-property :raw-value el))
+                            (org-element-property :begin el))
+                            )
+                          nil t))
+         (end-zotero-headline (org-element-map data 'headline
+                          (lambda (el)
+                            (and (string= "zotero notes" (org-element-property :raw-value el))
+                            (org-element-property :end el))
+                            )
+                          nil t)))
+  (if begin-zotero-headline (delete-region begin-zotero-headline end-zotero-headline))
+  (save-excursion
+    (goto-char (point-max))
+    (insert "* zotero notes\n" notes))
+  )
+  )
+
+(defun riccardo/update-references ()
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let* ((start-reference (search-forward ":ROAM_REFS: @" nil t))
+           (end-reference (progn
+                            (forward-word)
+                            (point))))
+      (if (and start-reference end-reference)
+          (let* ((citekey (buffer-substring-no-properties start-reference end-reference))
+                 (zotero-notes (riccardo/grab-reference-items citekey '(note file)))
+                 )
+            (riccardo/create-zotero-notes (nth 0 zotero-notes) (nth 1 zotero-notes))
+                 )))))
+
+(add-hook 'find-file-hook #'riccardo/update-references)
+(add-hook 'after-save-hook #'riccardo/update-references)
+
+;; deal with zotero notes
 
 ;; OTHER PERSONAL FUNCTIONS
 ;;
