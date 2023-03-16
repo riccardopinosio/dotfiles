fc29f53ac8e08dafb45b78cc6c0e7e9050dc084c
Riccardo Pinosio <rpinosio@gmail.com>
2023-03-16 15:22:40 +0100

    more work on ansible for mac

---
+++ install_miniconda_mac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh --output ~/miniconda_install.sh
+chmod +x ~/miniconda_install.sh
+~/miniconda_install.sh -b
+rm ~/miniconda_install.sh
